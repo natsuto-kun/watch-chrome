59c95ccb43e65083f9846da9ed1da035c1bc18fa
中村夏都 <nakamuranatsu@nakamuranatsumiyakonoMacBook-Pro.local>
2021-12-28 19:54:58 +0900

    fix: manifest.jsonが取得出来ない問題の修正

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,6 @@
-"use strict";
+import jsonData from "../manifest.json" assert { type: "json" };
 
 chrome.tabs.query({active: true}, function(tabs) {
-    console.log(tabs);
+    console.log(tabs[0].url);
+    console.log(jsonData);
 });
