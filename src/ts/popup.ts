b773344c0c18acef810c13c9f969178d7566e2bb
中村夏都 <nakamuranatsu@nakamuranatsumiyakonoMacBook-Pro.local>
2022-01-01 14:45:09 +0900

    typescriptの導入

---
+++ src/ts/popup.ts
@@ -0,0 +1,29 @@
+async function localUrlData() {
+
+    const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+    const tab = tabs[0].url + "*";
+    console.log(tabs[0].id, tab)
+    await chrome.storage.local.set({key: tab});
+    const message = {
+        tab,
+        id: tabs[0].id
+    }
+    chrome.runtime.sendMessage(message, (res: any) => {
+        console.warn('callback', res)
+    })
+}
+
+window.addEventListener('load', () => {
+    localUrlData()
+})
+
+/*
+chrome.tabs.query({active: true, currentWindow: true}, aim_message);
+export function aim_message(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, tabs.url, callback);
+    return tabs.url;
+}
+function callback(text) {
+    alert(text);
+}
+*/
